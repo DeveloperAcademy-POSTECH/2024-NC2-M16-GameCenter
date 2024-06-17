d0dac3e862b4391249f7918832450b9713b142f0
YuBin Kim <yubin412@naver.com>
2024-06-17 22:48:54 +0900

    feat: 데이터 모델 구성

---
+++ BingoLingo/BingoLingo/Data/Restaurant.swift
@@ -0,0 +1,17 @@
+//
+//  Restaurant.swift
+//  BingoLingo
+//
+//  Created by KimYuBin on 6/17/24.
+//
+
+struct Restaurant {
+    var name: String
+    var location: String
+    var phone: String
+    var time: String
+    var menu: String
+    var latitude: Double
+    var longitude: Double
+    var isPhotoVerified: Bool
+}
