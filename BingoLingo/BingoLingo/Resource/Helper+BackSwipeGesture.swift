55aaa3fca39b5abc0d3ce437944133761ecfcea7
YuBin Kim <yubin412@naver.com>
2024-06-17 22:48:38 +0900

    feat: BackSwipeGesture extension 추가

---
+++ BingoLingo/BingoLingo/Resource/Helper+BackSwipeGesture.swift
@@ -0,0 +1,19 @@
+//
+//  Helper+BackSwipeGesture.swift
+//  BingoLingo
+//
+//  Created by KimYuBin on 6/17/24.
+//
+
+import UIKit
+
+extension UINavigationController: ObservableObject, UIGestureRecognizerDelegate {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+    
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
+}
